48d45086b203abfecf58280706cdb552135c34d3
David A. Wheeler <dwheeler@dwheeler.com>
2021-01-09 13:22:58 -0500

    Ignore LoadLibraryEx if LOAD_LIBRARY_SEARCH_SYSTEM32
    Ignore LoadLibraryEx if its third parameter is
    LOAD_LIBRARY_SEARCH_SYSTEM32, as this is safe.
    This eliminates a false positive.
    
    See:
    https://github.com/david-a-wheeler/flawfinder/issues/26
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- test/test.c
+++ test/test.c
@@ -77,6 +77,8 @@ demo2() {
   SetSecurityDescriptorDacl(&sd,TRUE,NULL,FALSE);
   /* This one is a bad idea - first param shouldn't be NULL */
   CreateProcess(NULL, "C:\\Program Files\\GoodGuy\\GoodGuy.exe -x", "");
+  /* This should be ignored */
+  (void) LoadLibraryEx(L"user32.dll", nullptr, LOAD_LIBRARY_SEARCH_SYSTEM32);
   /* Test interaction of quote characters */
   printf("%c\n", 'x');
   printf("%c\n", '"');
