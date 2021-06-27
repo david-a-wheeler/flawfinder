18b559b69d4aee71f6849c8b06706a306dd687ef
David A. Wheeler <dwheeler@dwheeler.com>
2021-06-27 16:32:16 -0400

    entrypoint.sh: Don't require output filename to be escaped
    Note that the input filenames still have to be escaped
    (to support the use of "-" options on the command line).
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
-# $1 arguments
+# $1 arguments. BEWARE: Some filenames may need to be escaped.
 # $2 output filename
 
-flawfinder $1 > $2
+flawfinder $1 > "$2"
 
 echo "Executed with success."
