904fafb5d1be774f24fcb5c882e57ff4915457a7
David A. Wheeler <dwheeler@dwheeler.com>
2021-06-29 18:42:10 -0400

    entrypoint.sh: Ensure exit code is from flawfinder
    Ensure that the returned exit code is from flawfinder, not
    another tool (like "tee").
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,10 @@
 # $1 whitespace-separated arguments. Some filenames may need to be escaped.
 # $2 output filename
 
-flawfinder $1 | tee "$2"
+output="${2:-flawfinder-output.txt}"
+
+flawfinder $1 > "$output"
+result="$?"
+
+cat "$output"
+exit "$result"
